47125193c375e48edeed108580712d2b33fc55ea
2004Moonlove <1079019149y@gmail.com>
2025-04-16 13:03:13 -0400

    card update

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function toggleDetail(id, btn) {
-    const detail = document.getElementById(id);
-    const isOpen = detail.style.display === "block";
-    detail.style.display = isOpen ? "none" : "block";
-    btn.innerText = isOpen ? "View Details" : "Hide Details";
-  }
-  
-  function toggleDetail(card) {
+function toggleDetail(card) {
     card.classList.toggle("open");
   }
   
